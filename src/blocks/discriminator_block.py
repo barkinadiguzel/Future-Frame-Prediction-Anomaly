96c59e224a9a19aa97f2ae5c666d41bf9f377aa3
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-12-19 22:31:35 +0300

    Create discriminator_block.py

---
+++ src/blocks/discriminator_block.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels=3):
+        super().__init__()
+
+        self.net = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(256, 512, kernel_size=4, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=1)
+        )
+
+    def forward(self, x):
+        return self.net(x)
